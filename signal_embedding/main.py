744248f44087ea992e87fa4360a99bed5da5e651
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2024-06-10 14:00:24 +0200

    Update test cli

--- signal_embedding/main.py
+++ signal_embedding/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from jsonargparse import CLI
+from jsonargparse import ArgumentParser
 
 from signal_embedding.controller import SignalEmbedder
 
@@ -8,50 +8,36 @@ config_dir = (Path(__file__).parent / "../configs").resolve()
 # config_file = "skorch_nn_Lee2019_MI.yaml"
 config_file = "jumping_means.yaml"
 
-
-def cli_signal_embedding() -> SignalEmbedder:
-    def aux(signal_embedder: SignalEmbedder):
-        return signal_embedder
-
-    return CLI(aux, default_config_files=[config_dir / config_file])
-
-
-def mockup_stream(stop_event):
-    import pylsl
-    import numpy as np
-    from dareplane_utils.general.time import sleep_s
-
-    # create mockup input stream:
-    info = pylsl.StreamInfo(
-        name="AODataStream",
-        type="EEG",
-        channel_count=3,
-        nominal_srate=128,
-    )
-    outlet = pylsl.StreamOutlet(info)
-
-    while not stop_event.is_set():
-        data = np.random.randn(
-            10,
-            2,
-        )
-        outlet.push_chunk(data)
-        sleep_s(.1)
-    return 0
-
-
 if __name__ == "__main__":
     import threading
     import time
     from signal_embedding.utils.logging import logger
+    from signal_embedding.utils.mockup import mockup_stream, mockup_marker_stream
 
     logger.setLevel("DEBUG")
 
-    embedder = cli_signal_embedding()
+    # cli = cli_signal_embedding()
+    cli = ArgumentParser(default_config_files=[config_dir / config_file])
+    cli.add_argument("--signal_embedder", type=SignalEmbedder)
+    cli.add_argument("--markers", action="store_true", default=False)
+    # args = cli.parse_args()
+    args = cli.parse_args([
+        "--signal_embedder.init_args.marker_stream_name=MarkersStream",
+        "--markers",
+    ])
+    args = cli.instantiate_classes(args)
+    embedder = args.signal_embedder
+    markers = args.markers
 
     mockup_event = threading.Event()
     mockup_thread = threading.Thread(target=mockup_stream, kwargs=dict(stop_event=mockup_event))
     mockup_thread.start()
+
+    if markers:
+        mockup_marker_event = threading.Event()
+        mockup_marker_thread = threading.Thread(
+            target=mockup_marker_stream, kwargs=dict(stop_event=mockup_marker_event))
+        mockup_marker_thread.start()
 
     embedder.init_all()
     embeder_thread, embedder_event = embedder.run()
---
+++ signal_embedding/utils/mockup.py
@@ -0,0 +1,41 @@
+import pylsl
+import numpy as np
+from dareplane_utils.general.time import sleep_s
+
+
+def mockup_stream(stop_event):
+    # create mockup input stream:
+    info = pylsl.StreamInfo(
+        name="AODataStream",
+        type="EEG",
+        channel_count=3,
+        nominal_srate=128,
+    )
+    outlet = pylsl.StreamOutlet(info)
+
+    while not stop_event.is_set():
+        data = np.random.randn(
+            10,
+            2,
+        )
+        outlet.push_chunk(data)
+        sleep_s(.1)
+    return 0
+
+
+def mockup_marker_stream(stop_event):
+    # create mockup input stream:
+    info = pylsl.StreamInfo(
+        name="MarkersStream",
+        type="Markers",
+        channel_count=1,
+        nominal_srate=pylsl.IRREGULAR_RATE,
+        channel_format="string",
+    )
+    outlet = pylsl.StreamOutlet(info)
+
+    while not stop_event.is_set():
+        mrk = "right_hand"
+        outlet.push_sample([mrk])
+        sleep_s(.1)
+    return 0
