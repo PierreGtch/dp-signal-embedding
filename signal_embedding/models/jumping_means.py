54d5fb639ae1a6bffea5b199aa4226cbbd8caeb1
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2025-02-20 12:06:22 +0100

    Reformat jumping means

--- signal_embedding/models/jumping_means.py
+++ signal_embedding/models/jumping_means.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import numpy as np
 from sklearn.preprocessing import FunctionTransformer
 
@@ -21,32 +22,32 @@ def jumping_means(X, intervals_samples: list[tuple[int, int]]):
     """
     assert X.ndim == 3
     means = np.concatenate(
-        [np.mean(X[:, :, start:end], axis=-1)
-         for start, end in intervals_samples],
+        [np.mean(X[:, :, start:end], axis=-1) for start, end in intervals_samples],
         axis=-1,
     )
     return means
 
 
+@dataclass
 class JumpingMeansTransformer(ModelGetter):
-    def __init__(self, intervals_seconds: list[tuple[float, float]]):
-        self.intervals_seconds = intervals_seconds
+    intervals_seconds: list[tuple[float, float]]
 
     def __call__(
-            self,
-            sfreq: float,
-            input_window_seconds: float,
-            chs_info: list[dict],
+        self,
+        sfreq: float,
+        input_window_seconds: float,
+        chs_info: list[dict],
     ):
-        intervals_samples = [(int(start * sfreq), int(end * sfreq))
-                             for start, end in self.intervals_seconds]
+        intervals_samples = [
+            (int(start * sfreq), int(end * sfreq))
+            for start, end in self.intervals_seconds
+        ]
         assert all(
-            (end <= input_window_seconds * sfreq) and
-            (start < end) and
-            (start >= 0)
+            (end <= input_window_seconds * sfreq) and (start < end) and (start >= 0)
             for start, end in intervals_samples
         )
         transformer = FunctionTransformer(
             jumping_means,
-            kw_args=dict(intervals_samples=intervals_samples), )
+            kw_args=dict(intervals_samples=intervals_samples),
+        )
         return transformer
